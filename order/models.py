d71cece255c3bacd15b6848e22c4ba7550cb6842
Patricia <patricia.magee@ittdublin.ie>
2019-10-24 13:29:26 +0100

    first commit

---
+++ order/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from shop.models import Product
+# Create your models here.
+
+class Order(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    emailAddress = models.EmailField(max_length=250, blank=True)
+
+    class Meta:
+        db_table = 'Order'
+        ordering = ('-created',)
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+    
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_items(self):
+        return OrderItem.objects.filter(order=self)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
+    product = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+        
---
+++ order/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.order_create, name='order_create'),
+    path('history/',views.order_history,name='order_history'),
+]
